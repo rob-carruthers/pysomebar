133910779e167fa738f1a68a34108436546fc3cc
Rob Carruthers <12486527+rob-carruthers@users.noreply.github.com>
2026-06-08 20:19:17 +0100

    Make updater and modules async

--- src/pysomebar/__init__.py
+++ src/pysomebar/__init__.py
@@ -1,7 +1,11 @@
+"""Main entry point for pysomebar."""
+
+import asyncio
 import os
 import random
 from pathlib import Path
 
+import aiofiles
 from .module import DateModule, Module
 
 XDG_RUNTIME_DIR = os.environ["XDG_RUNTIME_DIR"]
@@ -12,17 +16,60 @@ RETRIES = 10
 class SomebarUpdater:
     def __init__(self, separator: str = " | "):
         self.separator = separator
+        self._modules: list[Module] = []
         self.modules: list[Module] = []
         self.output: str = ""
         self.last_output: str = ""
+        self.update_event = asyncio.Event()
+        self.tasks: set[asyncio.Task] = set()
 
-    def write_output(self):
-        joined_output = " | ".join(module.output for module in self.modules)
-        with SOMEBAR.open("w") as f:
-            f.write(f"status {joined_output}\n")
+    async def add_module(self, module: Module) -> None:
+        """Add a module to this updater."""
+        if not module.enabled:
+            return
 
-    def initial_update(self):
-        [module.update() for module in self.modules]
+        module.updater = self
+        self.tasks.add(asyncio.create_task(module.loop()))
+        self._modules.append(module)
+
+    async def write_output(self) -> None:
+        """Write output to somebar's named pipe."""
+        joined_output = " | ".join(module.output for module in self._modules)
+
+        async with aiofiles.open(SOMEBAR, "w") as f:
+            status = f"status {joined_output}\n"
+            await f.write(status)
+
+        self.update_event.clear()
+
+    async def initial_update(self) -> None:
+        """Initialise state/output for all held modules."""
+        if len(self._modules) < 1:
+            return
+
+        for module in self._modules:
+            await module.update()
+
+        await self.write_output()
+
+    async def loop(self) -> None:
+        """Continuously wait for updates/interval timeouts and write to named pipe."""
+        if len(self._modules) < 1:
+            return
+
+        while True:
+            await self.update_event.wait()
+            await asyncio.sleep(0.02)
+            await self.write_output()
+
+
+async def main_loop() -> None:
+    """Start main async loop."""
+    updater = SomebarUpdater()
+    await updater.add_module(DateModule(1))
+    await updater.add_module(RandomModule(1.5))
+    await updater.initial_update()
+    await updater.loop()
 
 
 def main() -> None:
@@ -32,7 +79,4 @@ def main() -> None:
     else:
         raise FileNotFoundError
 
-    updater = SomebarUpdater()
-    updater.modules.append(DateModule(5))
-    updater.initial_update()
-    updater.write_output()
+    asyncio.run(main_loop())
--- src/pysomebar/module.py
+++ src/pysomebar/module.py
@@ -1,22 +1,54 @@
+"""Modules for pysomebar."""
+
+import asyncio
 import datetime
 from abc import ABC, abstractmethod
+from typing import TYPE_CHECKING
 
 from .config import CONFIG
 
+if TYPE_CHECKING:
+    from . import SomebarUpdater
+
 
 class Module(ABC):
-    def __init__(self, interval: int) -> None:
+    """Abstract base class for modules."""
+
+    def __init__(self, interval: int) -> None:  # noqa: D107
+        self.updater: SomebarUpdater | None = None
         self.interval = interval
         self.output: str = ""
+        self.enabled: bool = False
 
     @abstractmethod
-    def update(self) -> None: ...
+    async def update(self) -> None:
+        """Abstract method for updating asynchronously."""
+        ...
+
+    async def loop(self) -> None:
+        if not self.enabled:
+            return
+
+        while True:
+            try:
+                await asyncio.wait_for(self.update(), timeout=self.interval)
+            except asyncio.TimeoutError:  # noqa: UP041
+                continue
+            await asyncio.sleep(self.interval)
 
 
 class DateModule(Module):
     def __init__(self, interval: int):
         super().__init__(interval)
 
-    def update(self) -> None:
+        self.enabled = CONFIG.date.enabled
+
+    async def update(self) -> None:
+        """Update output with current date/time in chosen format."""
+        if not self.enabled:
+            return
+
         now = datetime.datetime.now().astimezone().strftime(CONFIG.date.format)
         self.output = now
+        if self.updater is not None:
+            self.updater.update_event.set()
