a703dd54d7296bc104207d16f6925d21a7c072b7
Rob Carruthers <12486527+rob-carruthers@users.noreply.github.com>
2026-08-06 13:48:45 +0100

    Get data from mpd and pacman modules in picostatus

--- src/pysomebar/picostatus.py
+++ src/pysomebar/picostatus.py
@@ -1,17 +1,24 @@
 import asyncio
 import datetime
 import json
+from typing import Literal
 
 import serial_asyncio
+
+from pysomebar.module import Module, MPDModule, PacmanModule
+
+PicoStatusInputDataType = Literal["time", "mpd", "pacman"]
 
 
 class PicoStatusUpdater:
     def __init__(
         self,
+        modules: dict[str, Module],
         port: str = "/dev/ttyACM1",
         baud: int = 115200,
         interval_secs: float = 0.5,
-    ):
+    ) -> None:
+        self.modules = modules
         self.port = port
         self.baud = baud
         self.interval = interval_secs
@@ -24,15 +31,36 @@ class PicoStatusUpdater:
             baudrate=self.baud,
         )
 
-    def format_status(self) -> str:
+    def get_mpd_data(self) -> tuple[str, int, int]:
+        mpd_module = self.modules.get("mpd")
+        if not isinstance(mpd_module, MPDModule) or mpd_module.status is None:
+            return "mpd not loaded", 0, 100
+
+        status = mpd_module.status
+        if status.state == "stop":
+            return "Stopped", status.pos, status.dur
+
+        now_playing = status.artist + " - " + status.title
+        return now_playing, status.pos, status.dur
+
+    def get_pacman_data(self) -> str:
+        pacman_module = self.modules.get("pacman")
+        if not isinstance(pacman_module, PacmanModule):
+            return "No network!"
+
+        return pacman_module.raw_output
+
+    def format_status(self) -> dict[PicoStatusInputDataType, dict[str, str | int]]:
 
         now = datetime.datetime.now(tz=datetime.UTC).astimezone()
         nowstr = now.strftime("%H:%M:%S")
+        mpd_now_playing, pos, dur = self.get_mpd_data()
+        pacman_updates = self.get_pacman_data()
 
         return {
             "time": {"text": nowstr},
-            "mpd": {"text": "EA Games Soundtrack - Lakuwani", "dur": "191", "pos": "126"},
-            "pacman": {"text": "6 updates"},
+            "mpd": {"text": mpd_now_playing, "dur": dur, "pos": pos},
+            "pacman": {"text": pacman_updates},
         }
 
     async def run(self) -> None:
