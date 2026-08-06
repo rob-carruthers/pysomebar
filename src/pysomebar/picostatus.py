77f08b3af846d4cd2614b6bf495a2038bb4598dc
Rob Carruthers <12486527+rob-carruthers@users.noreply.github.com>
2026-08-06 15:35:18 +0100

    Inject MPD player state into picostatus output

--- src/pysomebar/picostatus.py
+++ src/pysomebar/picostatus.py
@@ -6,6 +6,7 @@ from typing import Literal
 import serial_asyncio
 
 from pysomebar.module import Module, MPDModule, PacmanModule
+from pysomebar.module.mpd import MPDPlayerState
 
 PicoStatusInputDataType = Literal["time", "mpd", "pacman"]
 
@@ -31,17 +32,17 @@ class PicoStatusUpdater:
             baudrate=self.baud,
         )
 
-    def get_mpd_data(self) -> tuple[str, int, int]:
+    def get_mpd_data(self) -> tuple[str, MPDPlayerState, int, int]:
         mpd_module = self.modules.get("mpd")
         if not isinstance(mpd_module, MPDModule) or mpd_module.status is None:
-            return "mpd not loaded", 0, 100
+            return "mpd not loaded", "stop", 0, 100
 
         status = mpd_module.status
         if status.state == "stop":
-            return "Stopped", status.pos, status.dur
+            return "Stopped", "stop", status.pos, status.dur
 
         now_playing = status.artist + " - " + status.title
-        return now_playing, status.pos, status.dur
+        return now_playing, status.state, status.pos, status.dur
 
     def get_pacman_data(self) -> str:
         pacman_module = self.modules.get("pacman")
@@ -54,12 +55,12 @@ class PicoStatusUpdater:
 
         now = datetime.datetime.now(tz=datetime.UTC).astimezone()
         nowstr = now.strftime("%H:%M:%S")
-        mpd_now_playing, pos, dur = self.get_mpd_data()
+        mpd_now_playing, state, pos, dur = self.get_mpd_data()
         pacman_updates = self.get_pacman_data()
 
         return {
             "time": {"text": nowstr},
-            "mpd": {"text": mpd_now_playing, "dur": dur, "pos": pos},
+            "mpd": {"text": mpd_now_playing, "state": state, "dur": dur, "pos": pos},
             "pacman": {"text": pacman_updates},
         }
 
